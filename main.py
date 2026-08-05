d86252794c9a425737d4b6e96cf42f7e523c3615
Jean-Simon-de-Witt <jeansimon.dewitt@gmail.com>
2026-08-05 18:16:22 +0200

    Refactured main file to prepare for UI development.

--- main.py
+++ main.py
@@ -1,32 +1,39 @@
-import json
+# Dependencies
+import sys
 import os
 from dotenv import load_dotenv
+from PyQt6.QtWidgets import QApplication
+
+# Backend classes
 from classes.invgate.invgate_connection import InvgateConnection
 from classes.invgate.invgate_routes import InvgateRoutes as routes
-from classes.invgate.invgate_user import InvgateUser
-from classes.invgate.invgate_asset import InvgateAsset
+
+# Frontend classes
 
 # Load Credentials
-load_dotenv()
+def main():
 
-DOMAIN = os.getenv("INVGATE_DOMAIN")
-CLIENT_ID = os.getenv("INVGATE_CLIENT_ID")
-CLIENT_SECRET = os.getenv("INVGATE_CLIENT_SECRET")
+    load_dotenv()
+    DOMAIN = os.getenv("INVGATE_DOMAIN")
+    CLIENT_ID = os.getenv("INVGATE_CLIENT_ID")
+    CLIENT_SECRET = os.getenv("INVGATE_CLIENT_SECRET")
 
-if not all([DOMAIN, CLIENT_ID, CLIENT_SECRET]):
-    raise ValueError("Missing API credentials. Check your .env file.")
-
-
-invgate = InvgateConnection(DOMAIN, CLIENT_ID, CLIENT_SECRET)
-
-if invgate.access_token:
-    test_user = invgate.get_user(name = "Invgate Test User Edited")
-
-    print(test_user.to_string())
-    # response = invgate.update_user(test_user)
-
-    # print(response.to_string())
+    if not all([DOMAIN, CLIENT_ID, CLIENT_SECRET]):
+        raise ValueError("Missing API credentials. Check your .env file.")
 
 
+    invgate = InvgateConnection(DOMAIN, CLIENT_ID, CLIENT_SECRET)
 
-        
+    if not invgate.access_token:
+        print("Failed to authenticate with InvGate. Exiting...")
+        sys.exit(1)
+
+    app = QApplication(sys.argv) 
+
+    window = MainWindow(api_connection=invgate)
+    window.show()
+
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()     
